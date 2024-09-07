062dbc7033209c54a9d74176e827cb6f9bf8d523
Oleh Kozlovskiy <oleg.kozlovskiy.35p@gmail.com>
2024-09-07 14:51:55 +0300

    Code after refactoring

--- BrainfuckBasicCommands.cs
+++ BrainfuckBasicCommands.cs
@@ -9,50 +9,74 @@ namespace func.brainfuck
 {
 	public class BrainfuckBasicCommands
 	{
-		public delegate void RegisterDelegate(char start, char end, IVirtualMachine vm);
-
 		public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
 		{
-			vm.RegisterCommand('.', b => { write.Invoke(Convert.ToChar(b.Memory[b.MemoryPointer]));});
-			vm.RegisterCommand('+', b => 
-			{
-				if (vm.Memory[vm.MemoryPointer] > Byte.MaxValue-1)
-					vm.Memory[vm.MemoryPointer] = 0;
-				else
+			RegisterWriteLine(vm, write);
+			RegisterReadLine(vm, read);
+			RegisterIncrement(vm);
+			RegisterDecrement(vm);
+			RegisterNext(vm);
+            RegisterPrevious(vm);
+            RegisterLettersAndNumbers('0', '9', vm);
+            RegisterLettersAndNumbers('A', 'Z', vm);
+			RegisterLettersAndNumbers('a', 'z', vm);
+		}
+
+		private static void RegisterWriteLine(IVirtualMachine vm, Action<char> write)
+		{
+            vm.RegisterCommand('.', b => { write.Invoke(Convert.ToChar(b.Memory[b.MemoryPointer])); });
+        }
+	
+		private static void RegisterReadLine(IVirtualMachine vm, Func<int> read)
+		{
+            vm.RegisterCommand(',', b => { vm.Memory[b.MemoryPointer] = Convert.ToByte(read.Invoke()); });
+        }
+
+		private static void RegisterIncrement(IVirtualMachine vm)
+		{
+            vm.RegisterCommand('+', b =>
+            {
+                if (vm.Memory[vm.MemoryPointer] > Byte.MaxValue - 1)
+                    vm.Memory[vm.MemoryPointer] = 0;
+                else
                     vm.Memory[vm.MemoryPointer]++;
             });
+        }
 
-			vm.RegisterCommand('-', b =>
+		private static void RegisterDecrement(IVirtualMachine vm)
+		{
+            vm.RegisterCommand('-', b =>
             {
                 if (vm.Memory[vm.MemoryPointer] <= byte.MinValue)
                     vm.Memory[vm.MemoryPointer] = Byte.MaxValue;
                 else
                     vm.Memory[vm.MemoryPointer]--;
             });
+        }
 
-            vm.RegisterCommand(',', b => { vm.Memory[b.MemoryPointer] = Convert.ToByte(read.Invoke()); });
-			vm.RegisterCommand('>', b => 
-			{
-				if (vm.MemoryPointer >= vm.Memory.Length-1)
-					vm.MemoryPointer = 0;
-				else
-					vm.MemoryPointer++; 
-			});
+        private static void RegisterNext(IVirtualMachine vm)
+        {
+            vm.RegisterCommand('>', b =>
+            {
+                if (vm.MemoryPointer >= vm.Memory.Length - 1)
+                    vm.MemoryPointer = 0;
+                else
+                    vm.MemoryPointer++;
+            });
+        }
 
-			vm.RegisterCommand('<', b => 
-			{ 
-				if(vm.MemoryPointer <= 0)
-					vm.MemoryPointer = vm.Memory.Length - 1;
-				else
-					vm.MemoryPointer--;
-			});
+		private static void RegisterPrevious(IVirtualMachine vm)
+		{
+            vm.RegisterCommand('<', b =>
+            {
+                if (vm.MemoryPointer <= 0)
+                    vm.MemoryPointer = vm.Memory.Length - 1;
+                else
+                    vm.MemoryPointer--;
+            });
+        }
 
-			RegisterDelegate registerSymbols = RegisterLettersAndNumbers;
-			registerSymbols('0', '9', vm);
-			registerSymbols('A', 'Z', vm);
-			registerSymbols('a', 'z', vm);
-		}
-		public static void RegisterLettersAndNumbers(char startSymbol, char endSymbol, IVirtualMachine vm)
+        private static void RegisterLettersAndNumbers(char startSymbol, char endSymbol, IVirtualMachine vm)
 		{
 			for (char i = startSymbol; i <= endSymbol; i++)
 			{
