fefe724cdceafe6b17335388cdc30caffa8c53f4
Oleh Kozlovskiy <oleg.kozlovskiy.35p@gmail.com>
2024-09-07 14:31:32 +0300

    Release 1.1

--- BrainfuckBasicCommands.cs
+++ BrainfuckBasicCommands.cs
@@ -9,7 +9,8 @@ namespace func.brainfuck
 {
 	public class BrainfuckBasicCommands
 	{
-		// при отримані Program через конструктор всі ASCII символи потрібно конвертувати в byte і зберегти в Memory
+		public delegate void RegisterDelegate(char start, char end, IVirtualMachine vm);
+
 		public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
 		{
 			vm.RegisterCommand('.', b => { write.Invoke(Convert.ToChar(b.Memory[b.MemoryPointer]));});
@@ -30,9 +31,34 @@ namespace func.brainfuck
             });
 
             vm.RegisterCommand(',', b => { vm.Memory[b.MemoryPointer] = Convert.ToByte(read.Invoke()); });
-			vm.RegisterCommand('>', b => { vm.MemoryPointer++; });
-			vm.RegisterCommand('<', b => { vm.MemoryPointer--; });
-			//...
+			vm.RegisterCommand('>', b => 
+			{
+				if (vm.MemoryPointer >= vm.Memory.Length-1)
+					vm.MemoryPointer = 0;
+				else
+					vm.MemoryPointer++; 
+			});
+
+			vm.RegisterCommand('<', b => 
+			{ 
+				if(vm.MemoryPointer <= 0)
+					vm.MemoryPointer = vm.Memory.Length - 1;
+				else
+					vm.MemoryPointer--;
+			});
+
+			RegisterDelegate registerSymbols = RegisterLettersAndNumbers;
+			registerSymbols('0', '9', vm);
+			registerSymbols('A', 'Z', vm);
+			registerSymbols('a', 'z', vm);
+		}
+		public static void RegisterLettersAndNumbers(char startSymbol, char endSymbol, IVirtualMachine vm)
+		{
+			for (char i = startSymbol; i <= endSymbol; i++)
+			{
+				char tempChar = i;
+                vm.RegisterCommand(i, b => { vm.Memory[vm.MemoryPointer] = Convert.ToByte(tempChar); });
+            }
 		}
 	}
 }
--- VirtualMachine.cs
+++ VirtualMachine.cs
@@ -10,7 +10,7 @@ namespace func.brainfuck
 	{
 		public string Instructions { get; }
 		public int InstructionPointer { get; set; }
-		public byte[] Memory { get; } // напевно використовується для перетворення + в символ
+		public byte[] Memory { get; }
 		public int MemoryPointer { get; set; }
 		public Dictionary<char, Action<IVirtualMachine>> Register;
 
@@ -19,25 +19,11 @@ namespace func.brainfuck
 			Memory = new byte[memorySize];
 			Instructions = TransformToInstructions(program);
 			Register = new Dictionary<char, Action<IVirtualMachine>>();
-			//Memory = TransformInstructionsToByte(program, memorySize);
 		}
 
 		private string TransformToInstructions(string program) => Regex.Replace(program, "[^0-9a-zA-Z,.<>+-]+", "");
-	
-        private byte[] TransformInstructionsToByte(string program, int memorySize)
-        {
-            byte[] tempMemory = new byte[memorySize];
-            for (int i = 0; i < program.Length; i++)
-			{
-				tempMemory[i] = Convert.ToByte(program[i]);
-            }
-            return tempMemory;
-        }
 
-        public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
-		{
-			Register.Add(symbol, (IVirtualMachine) => execute(this));
-		}
+        public void RegisterCommand(char symbol, Action<IVirtualMachine> execute) => Register.Add(symbol, (IVirtualMachine) => execute(this));
 
         public void Run()
 		{
@@ -48,11 +34,6 @@ namespace func.brainfuck
                     var command = Instructions[InstructionPointer];
                     Register[command](this);
 				}
-				else
-				{
-                    MemoryPointer++;
-                    Memory[MemoryPointer] = Convert.ToByte(Instructions[InstructionPointer]);
-                }
                 InstructionPointer++;
             }
 		}
